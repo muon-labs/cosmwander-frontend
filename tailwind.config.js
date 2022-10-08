e73e73936485b88e9b80d67f1646de7a2886bda7
Kyaris <iris.ledesma.27@gmail.com>
2022-10-08 02:45:18 +0200

    add colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "cw-purple-300": "#E2BDFF",
+        "cw-purple-400": "#AF66E8",
+        "cw-purple-500": "#9341D4",
+        "cw-purple-600": "#691DA4",
+        "cw-light-red": "#E18780 ",
+        "cw-grey-950": "#141414",
+        "cw-grey-900": "#151416",
+        "cw-grey-850": "#1A191B",
+        "cw-grey-800": "#222222",
+        "cw-grey-700": "#383838",
+        "cw-grey-400": "#737373",
+        "cw-grey-600": "#8E8E8E",
+        "cw-grey-100": "#D2D2D2",
+      },
+    },
   },
   plugins: [],
 };
