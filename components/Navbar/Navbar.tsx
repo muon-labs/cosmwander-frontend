0503fbbc731a899960aa3a4430c4419b5da7b5c4
Kyaris <iris.ledesma.27@gmail.com>
2022-10-08 21:29:54 +0200

    add colors to navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,16 +1,58 @@
+import clsx from "clsx";
 import React from "react";
+import { useClient } from "../../providers/ClientProvider";
+import { motion } from "framer-motion";
+import CosmwanderLogo from "../Icons/ComswanderLogo";
+
+const CLASSES = {
+  osmosis: {
+    textFill: "fill-chain-osmosis-600",
+    iconFill: "fill-chain-osmosis-600",
+    from: "from-chain-osmosis-600/50",
+  },
+  juno: {
+    textFill: "fill-chain-juno-400",
+    iconFill: "fill-chain-juno-600",
+    from: "from-chain-juno-600/50",
+  },
+  stargaze: {
+    textFill: "fill-chain-stargaze-400",
+    iconFill: "fill-chain-stargaze-400",
+    from: "from-chain-stargaze-400/50",
+  },
+};
 
 const Navbar: React.FC = () => {
+  const { chain } = useClient();
+
   return (
-    <nav className="min-h-[4.5rem] p-4">
-      <div className="max-w-[1425px] mx-[auto] my-0 flex items-center justify-between py-4">
-        <img
-          src="/images/cosmwander.svg"
-          alt="CosmWander Logo"
-          className="h-[48px]"
+    <motion.nav
+      key={chain}
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: {
+          opacity: {
+            ease: "easeInOut",
+            duration: 0.5,
+          },
+        },
+      }}
+      className="min-h-[4.5rem] p-4 relative overflow-hidden"
+    >
+      <motion.div
+        className={clsx(
+          "absolute bg-gradient-radial via-transparent to-transparent top-[-50%] w-full h-[100%] blur-2xl",
+          CLASSES[chain].from
+        )}
+      ></motion.div>
+      <motion.div className="max-w-[1425px] mx-[auto] my-0 flex items-center justify-between py-4">
+        <CosmwanderLogo
+          iconFill={CLASSES[chain].iconFill}
+          textFill={CLASSES[chain].textFill}
         />
-      </div>
-    </nav>
+      </motion.div>
+    </motion.nav>
   );
 };
 
