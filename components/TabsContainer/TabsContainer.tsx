52d724009a5984e417b6f211e721ffce0f4ec1a9
Kyaris <iris.ledesma.27@gmail.com>
2022-10-09 19:12:18 +0200

    add tabs container

---
+++ components/TabsContainer/TabsContainer.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import clsx from "clsx";
+
+interface Props {
+  className?: string;
+  selectedTab: string;
+  options: { key: string; container: React.ReactElement }[];
+}
+
+const TabsContainer: React.FC<Props> = ({ selectedTab, options, className = "" }) => {
+  const ContainerTab = options.map(({ key, container }) => {
+    return <div key={key}>{selectedTab === key && container}</div>;
+  });
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <motion.div
+        key={selectedTab ? selectedTab : "empty"}
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: -10, opacity: 0 }}
+        transition={{ duration: 0.2 }}
+        className={className}
+      >
+        {ContainerTab}
+      </motion.div>
+    </AnimatePresence>
+  );
+};
+
+export default TabsContainer;
