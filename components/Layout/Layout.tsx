4650791ab71a69a0603706e86d2ecce793c65b37
Kyaris <iris.ledesma.27@gmail.com>
2022-10-11 21:10:48 +0200

    add padding to layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -6,7 +6,7 @@ const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="min-h-[100vh] flex flex-col">
       <Navbar />
-      <main className="max-w-[1425px] mx-[auto] my-0 flex-1 py-4 w-full">{children}</main>
+      <main className="max-w-[1425px] mx-[auto] px-4 2xl:px-0 lg my-0 flex-1 py-4 w-full">{children}</main>
       <Footer />
     </div>
   );
--- components/Layout/LayoutSearch.tsx
+++ components/Layout/LayoutSearch.tsx
@@ -7,7 +7,7 @@ const LayoutSearch: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="min-h-[100vh] flex flex-col">
       <NavbarSearch />
-      <main className="max-w-[1425px] mx-[auto] my-0 flex-1 py-4 w-full">{children}</main>
+      <main className="max-w-[1425px] mx-[auto] px-4 2xl:px-0 my-0 flex-1 py-4 w-full">{children}</main>
       <Footer />
     </div>
   );
