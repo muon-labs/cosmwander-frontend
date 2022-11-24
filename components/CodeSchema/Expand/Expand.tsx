ec3d83fa0e91ba554647711d47a2c0a176f13bcc
Kyaris <iris.ledesma.27@gmail.com>
2022-11-24 12:03:35 +0100

    fix expand all buttons

--- components/CodeSchema/Expand/Expand.tsx
+++ components/CodeSchema/Expand/Expand.tsx
@@ -9,17 +9,19 @@ interface Props {
 const Expand: React.FC<Props> = ({ color, setExpanded }) => {
   return (
     <div className="w-full mb-[20px] flex gap-[10px] items-center justify-end">
-      <button className="rounded-[4px] border border-cw-grey-600 bg-cw-grey-750 py-1 px-3 flex gap-2 items-center justify-center hover:bg-cw-grey-500">
+      <button
+        className="rounded-[4px] border border-cw-grey-600 bg-cw-grey-750 py-1 px-3 flex gap-2 items-center justify-center hover:bg-cw-grey-500"
+        onClick={() => setExpanded(true)}
+      >
         <PlusIcon color={`fill-chain-${color}-600`} />
-        <p className="text-sm" onClick={() => setExpanded(true)}>
-          Expand all
-        </p>
+        <p className="text-sm">Expand all</p>
       </button>
-      <button className="rounded-[4px] border border-cw-grey-600 bg-cw-grey-750 py-1 px-3 flex gap-2 items-center justify-center hover:bg-cw-grey-500">
+      <button
+        className="rounded-[4px] border border-cw-grey-600 bg-cw-grey-750 py-1 px-3 flex gap-2 items-center justify-center hover:bg-cw-grey-500"
+        onClick={() => setExpanded(false)}
+      >
         <MinusIcon color={`fill-chain-${color}-600`} />
-        <p className="text-sm" onClick={() => setExpanded(false)}>
-          Reset
-        </p>
+        <p className="text-sm">Reset</p>
       </button>
     </div>
   );
